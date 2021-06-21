3991d9b2b0b4263ab6ebd7b276de662c71f97103
zhanzhuang <542813934@qq.com>
2021-06-21 14:21:48 +0800

    log

--- src/test/java/com/example/autotest/AutoTestApplicationTests.java
+++ src/test/java/com/example/autotest/AutoTestApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.autotest;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.TestNG;
+
+import java.util.List;
 
 @SpringBootTest
 class AutoTestApplicationTests {
 
     @Test
     void contextLoads() {
+        String testngXmlPath = System.getProperty("user.dir") + "/target/test-classes/testng/testng.xml";
+        System.out.println("testngXmlPath:" + testngXmlPath);
+        TestNG testNG = new TestNG();
+        List<String> suites = Lists.newArrayList();
+        suites.add(testngXmlPath);
+        testNG.setTestSuites(suites);
+        testNG.run();
     }
 
 }
